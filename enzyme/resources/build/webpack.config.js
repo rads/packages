2fb668a76d4c862648eabf035cc6dd88896963eb
Radford Smith <contact@radsmith.com>
2017-11-16 09:55:37 -0800

    Remove react from being bundled with enzyme

--- enzyme/resources/build/webpack.config.js
+++ enzyme/resources/build/webpack.config.js
@@ -2,15 +2,15 @@ const path = require("path");
 const webpack = require("webpack");
 
 module.exports = {
-  entry: [path.join(__dirname, "node_modules", "enzyme", "build", "index.js"),
-          path.join(__dirname, "helper.js")],
+  entry: [path.join(__dirname, "helper.js")],
   output: {
     filename: "enzyme.bundled.js"
   },
   externals: {
-    "react/addons": true,
-    "react/lib/ExecutionEnvironment": true,
-    "react/lib/ReactContext": true,
-    "react-addons-test-utils": "react-dom"
+    "react": "React",
+    "react/lib/React": "React",
+    "react-dom": "ReactDOM",
+    "react-dom/server": "{}",
+    "react-dom/test-utils": "React.addons.TestUtils"
   }
 };
